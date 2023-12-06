f989d5c230259a14f7d7e1dabe40cf6053a3c5a5
Mateo PRIOUX <pmateo@bess-f2r2s18.clusters.42paris.fr>
2023-12-06 20:59:55 +0100

    some training with minilibx

--- INCLUDES/fractol.h
+++ INCLUDES/fractol.h
@@ -6,7 +6,7 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:28:00 by pmateo            #+#    #+#             */
-/*   Updated: 2023/12/01 03:16:50 by pmateo           ###   ########.fr       */
+/*   Updated: 2023/12/06 15:53:37 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include "mlx.h"
+#include <X11/X.h>
+#include <X11/keysym.h>
+#include "../minilibx-linux/mlx.h"
 
 
 
--- LAB/testminilib.c
+++ LAB/testminilib.c
@@ -6,49 +6,185 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 17:53:51 by pmateo            #+#    #+#             */
-/*   Updated: 2023/11/30 23:41:44 by pmateo           ###   ########.fr       */
+/*   Updated: 2023/12/06 20:45:10 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INCLUDES/fractol.h"
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// #include <X11/X.h>
+// #include <X11/keysym.h>
+// #include "mlx.h"
+
+#define WINDOW_WIDTH 600
+#define WINDOW_HEIGHT 300
+
+#define MLX_ERROR 1
+
+#define RED_PIXEL 0x00FF0000
+#define GREEN_PIXEL 0x0000FF00
+#define	WHITE_PIXEL 0x00FFFFFF
+
+typedef struct	s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
 
 typedef struct s_data
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	t_img	img;
 }	t_data;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+typedef struct s_rect
 {
-	char	*dst;
+	int x;
+	int	y;
+	int width;
+	int height;
+	int color;
+}	t_rect;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+int	handle_keypress(int keysm, t_data *data)
+{
+	if (keysm == XK_Escape)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return (0);
+}
+
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+	int		i;
+
+	i = img->bpp - 8;
+	pixel = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	while (i >= 0)
+	{
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+void	render_background(t_img *img, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < WINDOW_HEIGHT)
+	{
+		j = 0;
+		while (j < WINDOW_WIDTH)
+			img_pix_put(img, j++, i, color);
+		++i;
+	}
+}
+
+int	render_rect(t_img *img, t_rect rect)
+{
+	int	i;
+	int	j;
+
+	i = rect.y;
+	while (i < rect.y + rect.height)
+	{
+		j = rect.x;
+		while (j < rect.x + rect.width)
+			img_pix_put(img, j++, i, rect.color);
+		++i;
+	}
+	return (0);
+}
+
+int	render(t_data *data)
+{
+	if (data->win_ptr == NULL)
+		return (1);
+	render_background(&data->img, WHITE_PIXEL);
+	render_rect(&data->img, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	render_rect(&data->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
+	return (0);
+}
+
+int main(void)
+{
+	t_data data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "test");
+	if (data.win_ptr == NULL)
+	{
+		free(data.win_ptr);
+		return (MLX_ERROR);
+	}
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
+	mlx_loop_hook(data.mlx_ptr, &render, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+
+	mlx_loop(data.mlx_ptr);
+
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 }
 
 
+// typedef struct s_data
+// {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }	t_data;
 
-int	main()
-{
-	void	*mlx;
-	void	*win;
-	t_data	img;
-	int	x = 0;
-	int y = 0;
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+// {
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
+// }
+
+
+
+// int	main()
+// {
+// 	void	*mlx;
+// 	void	*win;
+// 	t_data	img;
+// 	int	x = 0;
+// 	int y = 0;
 	
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 1920, 1080, "mlx 42");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-																 &img.endian);
-	// printf("BITS PER PIXEL = %d\n", img.bits_per_pixel);
-	// printf("LINE LENGTH = %d\n", img.line_length);
-	my_mlx_pixel_put(&img, 960, 540, 0x00FF0000);
-	// printf("BITS PER PIXEL = %d\n", img.bits_per_pixel);
-	// printf("LINE LENGTH = %d\n", img.line_length);
-	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
-	mlx_loop(mlx);
-}
+// 	mlx = mlx_init();
+// 	win = mlx_new_window(mlx, 1920, 1080, "mlx 42");
+// 	img.img = mlx_new_image(mlx, 1920, 1080);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+// 																 &img.endian);
+// 	printf("BITS PER PIXEL = %d\n", img.bits_per_pixel);
+// 	printf("LINE LENGTH = %d\n", img.line_length);
+// 	if (img.endian != 0)
+// 		printf("ENDIANNESS = BE\n");
+// 	else
+// 		printf("ENDIANNESS = LE\n");
+// 	my_mlx_pixel_put(&img, 960, 540, 0x00FF0000);
+// 	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
+// 	mlx_loop(mlx);
+// }
