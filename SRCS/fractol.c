ce54fa802b67ad7affcfeee4775532b73bf0d9de
Mateo PRIOUX <pmateo@bess-f2r1s14.clusters.42paris.fr>
2024-02-18 22:21:11 +0100

    nothing

--- SRCS/fractol.c
+++ SRCS/fractol.c
@@ -6,17 +6,11 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:26:53 by pmateo            #+#    #+#             */
-/*   Updated: 2024/02/15 19:58:30 by pmateo           ###   ########.fr       */
+/*   Updated: 2024/02/18 20:22:16 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../INCLUDES/fractol.h"
-// #include <stdlib.h>
-// #include <stdio.h>
-
-// #include <X11/X.h>
-// #include <X11/keysym.h>
-// #include "mlx.h"
 
 #define WINDOW_WIDTH 600
 #define WINDOW_HEIGHT 300
@@ -27,7 +21,7 @@
 #define GREEN_PIXEL 0x0000FF00
 #define	WHITE_PIXEL 0x00FFFFFF
 
-typedef struct	s_img
+typedef struct s_img
 {
 	void	*mlx_img;
 	char	*addr;
@@ -43,14 +37,6 @@ typedef struct s_data
 	t_img	img;
 }	t_data;
 
-typedef struct s_rect
-{
-	int x;
-	int	y;
-	int width;
-	int height;
-	int color;
-}	t_rect;
 
 int	handle_keypress(int keysm, t_data *data)
 {
@@ -64,78 +50,30 @@ int	handle_keypress(int keysm, t_data *data)
 
 void	img_pix_put(t_data *data, int x, int y, int color)
 {
-	char	pixel;
-	int		x;
-	int		y;
-
-	x = 0;
-	y = 0;
-	if (data->img.bpp != 32)
-		color = mlx_get_color_value(data->mlx_ptr, color);
-	while (y < 360 && x < 640)
-	{
-		++y;
-		++x;
-		pixel = (y * data->img.line_len) + (x * 4);
-		if (data->img.endian == 1)
-		{
-			data->img.addr[pixel] = (color >> 24);
-			data->img.addr[pixel + 1] = (color >> 16) & 0xFF;
-			data->img.addr[pixel + 2] = (color >> 8) & 0xFF;
-			data->img.addr[pixel + 3] = (color) & 0xFF;
-		}
-		else if (data->img.endian == 0)
-		{
-			data->img.addr[pixel] = (color) & 0xFF;
-			data->img.addr[pixel + 1] = (color >> 8) & 0xFF;
-			data->img.addr[pixel + 2] = (color >> 16) & 0xFF;
-			data->img.addr[pixel + 3] = (color >> 24);
-		}
-	}
+	
 }
 
-int	render_rect(t_data *data, t_rect rect)
-{
-	int	i;
-	int	j;
-
-	i = rect.y;
-	while (i < rect.y + rect.height)
-	{
-		j = rect.x;
-		while (j < rect.x + rect.width)
-			img_pix_put(data, j++, i, rect.color);
-		++i;
-	}
-	return (0);
-}
 
 int	render(t_data *data)
 {
-	if (data->win_ptr == NULL)
-		return (1);
-	render_background(data, WHITE_PIXEL);
-	render_rect(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
-	render_rect(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
-	return (0);
+	if (data->win_ptr != NULL)
+		
 }
 
 int main(void)
 {
-	t_data data;
+	t_data	data;
 
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "test");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "ntm");
+	
 	if (data.win_ptr == NULL)
 	{
 		free(data.win_ptr);
 		return (MLX_ERROR);
 	}
-	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_len, &data.img.endian);
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 
